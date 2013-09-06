d1a8e9b29e8396c7a0c81aa208532114e43f4806
Tyler Morgan <tylermorgan86@gmail.com>
2013-09-06 16:52:27 -0700

    fixing square root

--- square_root.scm
+++ square_root.scm
@@ -7,7 +7,8 @@
   (define (improve guess x)
     (average guess (/ x guess)))
   (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
     guess
-    (sqrt-iter (improve guess x) x))
+    (sqrt-iter (improve guess x) x)))
   (sqrt-iter 1.0 x))
 
